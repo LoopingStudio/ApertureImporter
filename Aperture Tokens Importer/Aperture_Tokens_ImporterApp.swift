b4508a7404ad9d86f329a3b15b8dc52e2a7e1770
LoopingStudio <kelian.daste31@gmail.com>
2026-01-27 10:54:26 +0100

    Initial Commit

---
+++ Aperture Tokens Importer/Aperture_Tokens_ImporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aperture_Tokens_ImporterApp.swift
+//  Aperture Tokens Importer
+//
+//  Created by Kelian Daste on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Aperture_Tokens_ImporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aperture Tokens Importer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aperture Tokens Importer
+//
+//  Created by Kelian Daste on 27/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
